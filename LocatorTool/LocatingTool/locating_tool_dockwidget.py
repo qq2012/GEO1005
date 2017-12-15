d86a407b8c855ccc953a4460625434660dc65683
RobGeomatics <robdegrote@hotmail.com>
2017-12-15 11:46:39 +0100

    openfirebutton pycode
    Added code for the open fire button

--- LocatorTool/LocatingTool/locating_tool_dockwidget.py
+++ LocatorTool/LocatingTool/locating_tool_dockwidget.py
@@ -26,6 +26,8 @@ import os
 from PyQt4 import QtGui, uic
 from PyQt4.QtCore import pyqtSignal
 
+from . import utility_functions as uf
+
 FORM_CLASS, _ = uic.loadUiType(os.path.join(
     os.path.dirname(__file__), 'locating_tool_dockwidget_base.ui'))
 
@@ -34,7 +36,7 @@ class LocatingToolDockWidget(QtGui.QDockWidget, FORM_CLASS):
 
     closingPlugin = pyqtSignal()
 
-    def __init__(self, parent=None):
+    def __init__(self, iface, parent=None):
         """Constructor."""
         super(LocatingToolDockWidget, self).__init__(parent)
         # Set up the user interface from Designer.
@@ -44,7 +46,47 @@ class LocatingToolDockWidget(QtGui.QDockWidget, FORM_CLASS):
         # #widgets-and-dialogs-with-auto-connect
         self.setupUi(self)
 
+        #define globals
+        self.iface = iface
+        self.canvas = self.iface.mapCanvas()
+
+        # set up GUI operation signals
+        # data
+        # self.iface.projectRead.connect(self.updateLayers)
+        # self.iface.newProjectCreated.connect(self.updateLayers)
+        # self.iface.legendInterface().itemRemoved.connect(self.updateLayers)
+        # self.iface.legendInterface().itemAdded.connect(self.updateLayers)
+
+        self.openFireButton.clicked.connect(self.openFire)
+
+
+    def openFire(self,filename=""):
+        scenario_open = False
+        # scenario_file = os.path.join(u'/Users/jorge/github/GEO1005','sample_data','time_test.qgs')
+        # check if file exists
+        # if os.path.isfile(scenario_file):
+        #    self.iface.addProject(scenario_file)
+        #    scenario_open = True
+
+        last_dir = uf.getLastDir("SDSS")
+        new_file = QtGui.QFileDialog.getOpenFileName(self, "", last_dir, "(*.qgs)")
+        if new_file:
+            self.iface.addProject(unicode(new_file))
+            scenario_open = True
+        if scenario_open:
+            self.updateLayers()
+
+
     def closeEvent(self, event):
+        # disconnect interface signals
+        try:
+            self.iface.projectRead.disconnect(self.updateLayers)
+            self.iface.newProjectCreated.disconnect(self.updateLayers)
+            self.iface.legendInterface().itemRemoved.disconnect(self.updateLayers)
+            self.iface.legendInterface().itemAdded.disconnect(self.updateLayers)
+        except:
+            pass
+
         self.closingPlugin.emit()
         event.accept()
 
