c2ac15e33f48f6b8f78222bf47cefd4addf9c07d
Anna <anna.nordlov@gmail.com>
2018-01-11 22:07:48 +0100

    Cleanup, final selection
    restructured all methods, removed old code, removed loading of unneccesary layer and thus getlegendlayerbyname

--- LocatorTool/LocatingTool/locating_tool_dockwidget.py
+++ LocatorTool/LocatingTool/locating_tool_dockwidget.py
@@ -23,6 +23,7 @@
 
 import os
 import os.path
+import shutil
 
 from PyQt4 import QtGui, QtCore, uic
 from PyQt4.QtCore import pyqtSignal
@@ -85,7 +86,7 @@ class LocatingToolDockWidget(QtGui.QDockWidget, FORM_CLASS):
         self.plugin_dir = os.path.dirname(__file__)
         self.fire_layer = self.setFireLayer()
 
-
+    # Initial settings functions
     def warningLoadData(self):
         msgBox = QtGui.QMessageBox()
         msgBox.setText("All current layers will be dropped, continue?")
@@ -159,14 +160,11 @@ class LocatingToolDockWidget(QtGui.QDockWidget, FORM_CLASS):
             self.minDistLineEdit.setText(str(500))
             self.maxDistLineEdit.setText(str(1500))
 
-    def removeLegendLayer(self, layer):
-        if layer:
-            QgsMapLayerRegistry.instance().removeMapLayer(layer.id())
-
-    def clearMarked(self):
-        marked_layer = uf.getLegendLayerByName(self.iface, "Selection")
-        if marked_layer:
-            self.removeLegendLayer(marked_layer)
+    # analysis functions
+    def setSelectedAttribute(self):
+        layer = uf.getLegendLayerByName(self.iface, 'ok_areas_final')
+        fields = uf.getFieldNames(layer)
+        self.extractAttributeSummary(fields)
 
     def getMinBufferCutoff(self):
         cutoff = self.minDistLineEdit.text()
@@ -192,85 +190,14 @@ class LocatingToolDockWidget(QtGui.QDockWidget, FORM_CLASS):
         MinBuffer = processing.runalg('qgis:fixeddistancebuffer', layer, min_dist, 12, False, None)
 
         #create the donut (difference)
-        processing.runandload('qgis:symmetricaldifference', MaxBuffer['OUTPUT'], MinBuffer['OUTPUT'], None)
+        runalg = processing.runalg('qgis:symmetricaldifference', MaxBuffer['OUTPUT'], MinBuffer['OUTPUT'], '{}/analysis_data/donut.shp'.format(self.plugin_dir))
+        self.runalgShortcut(runalg, 'donut')
 
     def chooseWindDirection(self):
         choosetext = self.chooseWindDirectionCombo.currentText()
         direction = {'no wind': -1, 'N': 0, 'NE': 45, 'E': 90, 'SE': 135, 'S': 180, 'SW': 225, 'W': 270, 'NW': 315}
         WindDirection = direction[choosetext]
         return WindDirection
-
-    def selectFeaturesBuffer(self, layer=0):
-        if uf.getLegendLayerByName(self.iface, "Difference"):
-            intersect_layer = uf.getLegendLayerByName(self.iface, "Difference")
-        elif uf.getLegendLayerByName(self.iface, "Symmetrical difference"):
-            intersect_layer = uf.getLegendLayerByName(self.iface, "Symmetrical difference")
-
-        if layer and intersect_layer:
-            uf.selectFeaturesByIntersection(layer, intersect_layer, True)
-
-    def filterSelectionLayer(self, selection_layer):
-        routes_layer = uf. getLegendLayerByName(self.iface, 'Routes')
-
-        if not routes_layer:
-            filtered_layer = None
-        else:
-            dicti = processing.runalg('qgis:joinattributestable', selection_layer, routes_layer, 'FID2', 'to_FID', None)
-
-            join_path = dicti[dicti.keys()[0]] #this is the folder path to the layer
-            join_layer = QgsVectorLayer(join_path, "join_layer", "ogr")
-            # QgsMapLayerRegistry.instance().addMapLayers([join_layer])
-
-            idx = join_layer.fieldNameIndex('Area')
-            max_area = join_layer.maximumValue(idx)
-            required_area = int(self.areaEdit.text())
-            uf.selectFeaturesByRangeValues(join_layer,'Area',required_area,max_area)
-
-            area_dict = processing.runalg('qgis:saveselectedfeatures', join_layer, None)
-
-            area_layer = QgsVectorLayer(area_dict[area_dict.keys()[0]], 'selected_area', 'ogr')
-            # QgsMapLayerRegistry.instance().addMapLayers([area_layer])
-
-            sorted_features = uf.sortByField(area_layer, 'length')
-            top_nr = int(self.numOfResultsLineEdit.text())
-
-            return area_layer, sorted_features, top_nr
-
-    def selectTopLocations(self, locations_layer, sorted_features, top_nr=5):
-        top = sorted_features[0:top_nr]
-        top_fid = []
-        for feature in top:
-            top_fid.append(feature[3])
-
-        uf.selectFeaturesByListValues(locations_layer, 'FID2', top_fid)
-        top_dict = processing.runalg('qgis:saveselectedfeatures', locations_layer, None)
-        top_path = top_dict[top_dict.keys()[0]]
-
-        top_layer = QgsVectorLayer(top_path, "top_layer", "ogr")
-        # QgsMapLayerRegistry.instance().addMapLayers([top_layer])
-
-        return top_layer
-
-    def markAreas(self):
-        if uf.getLegendLayerByName(self.iface, "Difference"):
-            result_area = uf.getLegendLayerByName(self.iface, "Difference")
-        elif uf.getLegendLayerByName(self.iface, "Symmetrical difference"):
-            result_area = uf.getLegendLayerByName(self.iface, "Symmetrical difference")
-
-        ok_areas = uf.getLegendLayerByName(self.iface, "ok_areas_final")
-        #Check possibility of this function
-        if result_area and ok_areas:
-            self.selectFeaturesBuffer(ok_areas)
-            processing.runandload('qgis:saveselectedfeatures', ok_areas, None)
-            selection_layer = uf.getLegendLayerByName(self.iface, "Selection")
-            path = "%s/styles/" % QgsProject.instance().homePath()
-            processing.runalg('qgis:setstyleforvectorlayer', selection_layer, "%sok_areas_style.qml" % path)
-            ok_areas.removeSelection()
-
-        return selection_layer
-
-    def giveMessage(self):
-        self.iface.messageBar().pushMessage("test test:", "{}".format("testing"), level=0, duration=5)
 
     def calculateCone(self, firelayer=0):
         if self.chooseWindDirectionCombo.currentText() != 'no wind':
@@ -326,11 +253,8 @@ class LocatingToolDockWidget(QtGui.QDockWidget, FORM_CLASS):
         CRS = layer.crs().postgisSrid()
 
         URI = "Polygon?crs=epsg:" + str(CRS) + "&field=id:integer""&index=yes"
-
         # Create polygon layer for buffer
-        mem_layer = QgsVectorLayer(URI,
-                                   "Smoke cone",
-                                   "memory")
+        mem_layer = QgsVectorLayer(URI,"Smoke cone","memory")
 
         # add Map Layer to Registry
         QgsMapLayerRegistry.instance().addMapLayer(mem_layer)
@@ -367,12 +291,12 @@ class LocatingToolDockWidget(QtGui.QDockWidget, FORM_CLASS):
         self.removeLegendLayer(cone_width_layer)
 
     def biteFromDonut(self):
-        donut = uf.getLegendLayerByName(self.iface, "Symmetrical difference")
-        bite = uf.getLegendLayerByName(self.iface, "Smoke cone")
+        donut = QgsVectorLayer('{}/analysis_data/donut.shp'.format(self.plugin_dir), 'test', 'ogr')
+        bite = uf.getLegendLayerByName(self.iface, "Smoke cone") #TODO change!
         if donut and bite:
-            processing.runandload('qgis:difference', donut, bite, True, None)
+            self.runalgShortcut(processing.runalg('qgis:difference', donut, bite, True, '{}/analysis_data/donut_bite.shp'.format(self.plugin_dir)))
 
-    def defineFocalZone(self, layer=0):
+    def focusArea(self, layer=0):
         #create the buffers needed min and max
         min_dist = self.getMaxBufferCutoff()
         max_dist = 7500
@@ -383,48 +307,15 @@ class LocatingToolDockWidget(QtGui.QDockWidget, FORM_CLASS):
         processing.runandload('qgis:symmetricaldifference', MaxBuffer['OUTPUT'], MinBuffer['OUTPUT'], None)
 
         testname = uf.getLegendLayerByName(self.iface, "Symmetrical difference")
+
         path = "%s/styles/" % QgsProject.instance().homePath()
         processing.runalg('qgis:setstyleforvectorlayer', testname, "%sfocal_zone_style.qml" % path)
 
-    def everythingAtOnce(self):
-        firelayer = self.fire_layer
-        self.calculateDonut(firelayer)
-        self.calculateCone(firelayer)
-        self.biteFromDonut()
-        selection_lyr = self.markAreas()
-        if uf.getLegendLayerByName(self.iface, "Symmetrical difference"):
-            self.removeLegendLayer(uf.getLegendLayerByName(self.iface, "Symmetrical difference"))
-        if uf.getLegendLayerByName(self.iface, "Difference"):
-            self.removeLegendLayer(uf.getLegendLayerByName(self.iface, "Difference"))
-        self.defineFocalZone(firelayer)
-        self.calculateRoute()
-        area_layer, sorted_features, top_nr = self.filterSelectionLayer(selection_lyr)
-        top_layer = self.selectTopLocations(area_layer, sorted_features, top_nr)
-        self.extractAttributeSummary(top_layer)
-
-
-
-    def clearAllAnalysisLayers(self):
-        layers = ["Symmetrical difference", "Difference", "Smoke cone", "Selection", "Intersection", "Routes",
-                  "join_layer", "Regular points", "Mean coordinates", "selected_area"]
-        for layer_name in layers:
-            layer = uf.getLegendLayerByName(self.iface, layer_name)
-            if layer:
-                self.removeLegendLayer(layer)
-
-    def clearLegend(self):
-        legend = uf.getLegendLayers(self.iface)
-        for layer in legend:
-            self.removeLegendLayer(layer)
-
-    #############################
-    #   Network and route methods
-    #############################
-
+    # Network and route methods
     def getNetwork(self):
         roads_layer = uf.getLegendLayerByName(self.iface, 'Roads')
         if roads_layer:
-            # see if there is an obstacles layer to subtract roads from the network
+            # see if there is an obstacles layer to subtract roads from the network TODO: Do we want obstacles??
             obstacles_layer = uf.getLegendLayerByName(self.iface, "Obstacles")
             if obstacles_layer:
                 # retrieve roads outside obstacles (inside = False)
@@ -436,11 +327,11 @@ class LocatingToolDockWidget(QtGui.QDockWidget, FORM_CLASS):
                 road_network = roads_layer
             return road_network
 
-
     def buildNetwork(self):
         self.network_layer = self.getNetwork()
         if self.network_layer:
-            sources_layer = uf.getLegendLayerByName(self.iface, 'Selection')
+            sources_layer = QgsVectorLayer('{}/analysis_data/Selection.shp'.format(self.plugin_dir), 'Selection', 'ogr')
+
             source_points = [feature.geometry().centroid().asPoint() for feature in sources_layer.getFeatures()]
 
             fire = self.fire_layer
@@ -450,11 +341,10 @@ class LocatingToolDockWidget(QtGui.QDockWidget, FORM_CLASS):
             # build the graph including these points
             if len(source_points) > 1:
                 self.graph, self.tied_points = uf.makeUndirectedGraph(self.network_layer, source_points)
-        return
+        return sources_layer
 
     def calculateRoute(self):
-        self.buildNetwork() # instead of a buildNetwork-button
-        locations_layer = uf.getLegendLayerByName(self.iface, 'Selection')
+        locations_layer = self.buildNetwork() # instead of a buildNetwork-button
         locations_list = [feature.attribute('FID2') for feature in locations_layer.getFeatures()]
 
         # origin and destination must be in the set of tied_points
@@ -481,16 +371,95 @@ class LocatingToolDockWidget(QtGui.QDockWidget, FORM_CLASS):
             style_path = "%s/styles/" % QgsProject.instance().homePath()
             processing.runalg('qgis:setstyleforvectorlayer', routes_layer, "%sShortestRoute_style.qml" % style_path)
 
-    def refreshCanvas(self, layer):
-        if self.canvas.isCachingEnabled():
-            layer.setCacheImage(None)
+    def deleteRoutes(self): #TODO: implement this function? - maybe not needed since it is implemented in the 'clear-all'button?
+        routes_layer = uf.getLegendLayerByName(self.iface, "Routes")
+        if routes_layer:
+            ids = uf.getAllFeatureIds(routes_layer)
+            routes_layer.startEditing()
+            for id in ids:
+                routes_layer.deleteFeature(id)
+            routes_layer.commitChanges()
+
+    # Filtering and selection functions
+
+    def selectFeaturesBuffer(self, layer=0):
+        intersect_layer = QgsVectorLayer('{}/analysis_data/donut_bite.shp'.format(self.plugin_dir), 'donut', 'ogr')
+        if not intersect_layer:
+            intersect_layer = QgsVectorLayer('{}/analysis_data/donut.shp'.format(self.plugin_dir), 'donut', 'ogr')
+
+        # QgsMapLayerRegistry.instance().addMapLayers([intersect_layer])
+
+
+        if layer and intersect_layer:
+            uf.selectFeaturesByIntersection(layer, intersect_layer, True)
+
+    def markAreas(self):
+
+        result_area = QgsVectorLayer('{}/analysis_data/donut_bite.shp'.format(self.plugin_dir), 'donut', 'ogr')
+        if not result_area:
+            uf.showMessage(self.iface, 'no donut', dur=5)
+            result_area = QgsVectorLayer('{}/analysis_data/donut.shp'.format(self.plugin_dir), 'donut_bite', 'ogr')
+
+        ok_areas = uf.getLegendLayerByName(self.iface, "ok_areas_final")
+        # Check possibility of this function
+        if result_area and ok_areas:
+            self.selectFeaturesBuffer(ok_areas)
+            runalg = processing.runalg('qgis:saveselectedfeatures', ok_areas,
+                                       '{}/analysis_data/Selection.shp'.format(self.plugin_dir))
+            selection_layer = self.runalgShortcut(runalg, 'Selection')
+            # QgsMapLayerRegistry.instance().addMapLayers([selection_layer])
+            ok_areas.removeSelection()
+
+        return selection_layer
+
+    def filterSelectionLayer(self, selection_layer):
+        routes_layer = uf.getLegendLayerByName(self.iface, 'Routes')
+
+        if not routes_layer:
+            filtered_layer = None
         else:
-            self.canvas.refresh()
+            dicti = processing.runalg('qgis:joinattributestable', selection_layer, routes_layer, 'FID2', 'to_FID', None)
 
-    #################
-    #   Results tab
-    #################
+            join_path = dicti[dicti.keys()[0]]  # this is the folder path to the layer
+            join_layer = QgsVectorLayer(join_path, "join_layer", "ogr")
+            # QgsMapLayerRegistry.instance().addMapLayers([join_layer])
 
+            idx = join_layer.fieldNameIndex('Area')
+            max_area = join_layer.maximumValue(idx)
+            required_area = int(self.areaEdit.text())
+            uf.selectFeaturesByRangeValues(join_layer, 'Area', required_area, max_area)
+
+            area_dict = processing.runalg('qgis:saveselectedfeatures', join_layer, None)
+
+            area_layer = QgsVectorLayer(area_dict[area_dict.keys()[0]], 'selected_area', 'ogr')
+            QgsMapLayerRegistry.instance().addMapLayers([area_layer])
+
+            sorted_features = uf.sortByField(area_layer, 'length')
+
+
+            return area_layer, sorted_features
+
+    def selectTopLocations(self, locations_layer, sorted_features):
+        top_nr = int(self.numOfResultsLineEdit.text())
+        top = sorted_features[0:top_nr]
+        top_fid = []
+        for feature in top:
+            top_fid.append(feature[3])
+        uf.showMessage(self.iface, 'list {}'.format(top_fid))
+
+        uf.selectFeaturesByListValues(locations_layer, 'FID2', top_fid)
+        top_dict = processing.runalg('qgis:saveselectedfeatures', locations_layer, None)
+        top_path = top_dict[top_dict.keys()[0]]
+        top_layer = QgsVectorLayer(top_path, "Top locations", "ogr")
+        QgsMapLayerRegistry.instance().addMapLayers([top_layer])
+        self.removeLegendLayer(locations_layer)
+
+        path = "%s/styles/" % QgsProject.instance().homePath()
+        processing.runalg('qgis:setstyleforvectorlayer', top_layer, "%stop_style.qml" % path)
+
+        return top_layer
+
+    # Reporting functions
     def extractAttributeSummary(self, layer):
         # get summary of the attribute
         fields = uf.getFieldNames(layer)
@@ -506,7 +475,6 @@ class LocatingToolDockWidget(QtGui.QDockWidget, FORM_CLASS):
         self.clearTable()
         self.updateTable(summary)
 
-    # table window functions
     def updateTable(self, values):
         self.statisticsTable.setColumnCount(3)
         self.statisticsTable.setHorizontalHeaderLabels(["Landuse", "Size", "Distance"])
@@ -526,8 +494,73 @@ class LocatingToolDockWidget(QtGui.QDockWidget, FORM_CLASS):
     def clearTable(self):
         self.statisticsTable.clear()
 
+    # help functions
+    def runalgShortcut(self, dicti, name='layer', memory=False, load=False):
+        path = dicti[dicti.keys()[0]]
+        if not memory:
+            provider = 'ogr'
+        else:
+            provider = 'memory'
+
+        layer = QgsVectorLayer(path, name, provider)
+
+        if load:
+            QgsMapLayerRegistry.instance().addMapLayers([layer])
+
+        return layer
+
+    def giveMessage(self):
+        self.iface.messageBar().pushMessage("test test:", "{}".format("testing"), level=0, duration=5)
+
+    # clearing functions
+    def removeLegendLayer(self, layer):
+        if layer:
+            QgsMapLayerRegistry.instance().removeMapLayer(layer.id())
+
+    def clearLegend(self):
+        legend = uf.getLegendLayers(self.iface)
+        for layer in legend:
+            self.removeLegendLayer(layer)
+
+    def clearAllAnalysisLayers(self):
+        layers = ["Symmetrical difference", "Routes","join_layer", "Regular points", "Mean coordinates",
+                  "selected_area", "Smoke cone", "Top locations"]
+        for layer_name in layers:
+            layer = uf.getLegendLayerByName(self.iface, layer_name)
+            if layer:
+                self.removeLegendLayer(layer)
+
+    def clearAnalysisDataFolder(self):
+        folder = '{}/analysis_data/'.format(self.plugin_dir)
+        for the_file in os.listdir(folder):
+            file_path = os.path.join(folder, the_file)
+            try:
+                if os.path.isfile(file_path):
+                    os.unlink(file_path)
+            except Exception as e:
+                uf.showMessage(self.iface, 'OBS analysis_data folder not cleared completely!', lev=2, dur=5)
+
+    # The BIG button function
+    def everythingAtOnce(self):
+        if self.selectFireButton.currentIndex() == 0:
+            uf.showMessage(self.iface, 'OBS Please select a fire scenario', lev=2, dur=5)
+        else:
+            self.clearAnalysisDataFolder()
+            firelayer = self.fire_layer
+            self.calculateDonut(firelayer)
+            self.calculateCone(firelayer)
+            self.biteFromDonut()
+            selection_lyr = self.markAreas()
+            self.focusArea(firelayer)
+            self.calculateRoute()
+            area_layer, sorted_features = self.filterSelectionLayer(selection_lyr)
+            top_layer = self.selectTopLocations(area_layer, sorted_features)
+            self.extractAttributeSummary(top_layer)
+
     def closeEvent(self, event):
-        self.clearAllAnalysisLayers()
+        # self.clearAllAnalysisLayers()
+        #TODO clear analysis_data folder
+        self.clearAnalysisDataFolder()
         self.closingPlugin.emit()
         event.accept()
 
